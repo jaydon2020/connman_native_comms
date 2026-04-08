ea5b493d647b45c6e066f83c5091eb526a0a58bd
JianDe <jiande2020@protonmail.com>
2026-04-08 21:43:15 +0800

    chore(scaffold): initialize project structure
    Set up the Dart package structure base on bluez_native_comms
    add project scaffold and pub.dev skeleton
    
    Signed-off-by: Jian De <jiande2020@protonmail.com>

---
+++ lib/connman_native_comms.dart
@@ -0,0 +1,3 @@
+/// High-performance Connection Manager (ConnMan) client for Linux using native_comms
+/// and sdbus-cpp.
+library connman_native_comms;
---
+++ test/connman_test.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unused_import
+
+import 'package:connman_native_comms/connman_native_comms.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('initial setup', () {
+    expect(true, isTrue);
+  });
+}
